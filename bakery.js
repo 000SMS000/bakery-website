54e13b944f3aa251c34e032c79083cf0012c5dfe
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2026-02-04 22:26:21 +0200

    finesh

--- bakery.js
+++ bakery.js
@@ -78,3 +78,32 @@ filterBtns.forEach(btn => {
     });
   });
 });
+
+// contact
+const form = document.getElementById("contactForm");
+const successMsg = document.getElementById("successMsg");
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const subject = document.getElementById("subject").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!name || !email || !subject || !message) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    alert("Invalid email format");
+    return;
+  }
+
+  successMsg.textContent = "Message sent successfully!";
+  successMsg.style.color = "green";
+  form.reset();
+});
